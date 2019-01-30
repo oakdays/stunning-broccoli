3bd08054093070094cd407cb78c36460de2c75b9
Iury Dias <iury.dias1@gmail.com>
2019-01-30 22:27:35 +0100

    First actual commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <wrapper>
+      <styled-title>:)</styled-title>
+    </wrapper>
+  </div>
+</template>
+
+<script>
+import { Wrapper, StyledTitle } from './components/styled'
+
+export default {
+  name: 'App',
+  components: {
+    Wrapper,
+    StyledTitle
+  }
+}
+</script>
