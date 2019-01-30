b34d3d4e9a1ac223a520633717c55f6a35b9384c
Iury Dias <iury.dias1@gmail.com>
2019-01-30 22:47:58 +0100

    Create sample styled button

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <wrapper>
       <styled-title>:)</styled-title>
+      <styled-button>Click me</styled-button>
     </wrapper>
   </div>
 </template>
 
 <script>
 import { Wrapper, StyledTitle } from './components/styled'
+import StyledButton from './components/styled/StyledButton'
 
 export default {
   name: 'App',
   components: {
     Wrapper,
-    StyledTitle
+    StyledTitle,
+    StyledButton
   }
 }
 </script>
